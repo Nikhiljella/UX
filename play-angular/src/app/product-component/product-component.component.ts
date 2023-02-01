aa22302fdc1fa6f95ea1f2357e65bf728354408e
jella nikhil babu <nikhilpateljella@gmail.com>
2023-02-01 08:44:28 +0530

    product component added and styles

---
+++ play-angular/src/app/product-component/product-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'prod-component',
+  templateUrl: './product-component.component.html',
+  styleUrls: ['./product-component.component.css']
+})
+export class ProductComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
